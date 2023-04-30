521ec4110756100980a26cfc34587f0053048419
SalmaQassem <salma99kasem@gmail.com>
2023-04-30 03:05:26 +0300

    Added Slider to Landing Page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 import "./HomePage.css";
 import { useState } from "react";
-import background from "../../images/hero-bg.webp";
 import NavBar from "./NavBar";
 import Cart from "../Cart/Cart";
 import Landing from "./Landing";
@@ -15,9 +14,6 @@ const HomePage = () => {
   };
   return (
     <section className="home" id="Home">
-      <div className="bg-Image">
-        <img src={background} alt="" />
-      </div>
       <NavBar cartOpened={isCartOpenedHandler} />
       <Cart cartState={isCartOpened} cartClosed={isCartClosedHandler} />
       <Landing />
--- src/components/HomePage/Landing.js
+++ src/components/HomePage/Landing.js
@@ -1,35 +1,105 @@
 import "./Landing.css";
 import React from "react";
 import Container from "../UI/Container";
+import Carousel from "react-multi-carousel";
 import Button from "../UI/Button";
-
+const responsive = {
+  desktop: {
+    breakpoint: {
+      max: 3000,
+      min: 1024,
+    },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: {
+      max: 464,
+      min: 0,
+    },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: {
+      max: 1024,
+      min: 464,
+    },
+    items: 1,
+  },
+};
+const LandingItems = [
+  {
+    id: 0,
+    title: "fast food restaurant",
+    description:
+      "Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.",
+  },
+  {
+    id: 1,
+    title: "fast food restaurant",
+    description:
+      "Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.",
+  },
+  {
+    id: 2,
+    title: "fast food restaurant",
+    description:
+      "Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.",
+  },
+];
 const Landing = () => {
+  const CustomDot = ({ onClick, ...rest }) => {
+    const { active } = rest;
+    return (
+      <button
+        className={active ? "dots-item  active" : "dots-item  inactive"}
+        onClick={() => onClick()}
+      ></button>
+    );
+  };
   return (
     <section className="landing">
-      <div className="slider">
-        <div className="slide">
-          <Container>
-            <div className="text">
-              <h1>fast food restaurant</h1>
-              <p>
-                Doloremque, itaque aperiam facilis rerum, commodi, temporibus
-                sapiente ad mollitia laborum quam quisquam esse error unde.
-                Tempora ex doloremque, labore, sunt repellat dolore, iste magni
-                quos nihil ducimus libero ipsam.
-              </p>
-              <Button className="landing-btn" href="#Shop">
-                order now
-              </Button>
-            </div>
-          </Container>
-        </div>
-      </div>
       <Container>
-        <ul className="dots-list">
-          <li className="active"></li>
-          <li></li>
-          <li></li>
-        </ul>
+        <Carousel
+          additionalTransfrom={0}
+          arrows={false}
+          autoPlay
+          autoPlaySpeed={6000}
+          centerMode={false}
+          className=""
+          containerClass="container"
+          customDot={<CustomDot />}
+          dotListClass="carousel-dots"
+          draggable={false}
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          responsive={responsive}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={true}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          {LandingItems.map((item) => {
+            return (
+              <div className="text" key={item.id}>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <Button className="landing-btn" href="#Shop">
+                  order now
+                </Button>
+              </div>
+            );
+          })}
+        </Carousel>
       </Container>
     </section>
   );
